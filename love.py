f7d2e1f94b564143f26e51dfdf336999a457e7ae
Mink Alex Vina <64893437+anh-dz@users.noreply.github.com>
2021-02-14 17:03:38 +0700

    Update love.py

--- love.py
+++ love.py
@@ -17,9 +17,18 @@ love = ["________00000000000___________000000000000_________",
 "_________________000_________000___________________",
 "_________________ __000_____00_____________________",
 "______________________00__00_______________________",
-"________________________00_________________________",]
+"________________________00_________________________",
+""]
+
+word = " Tình yêu là một thứ kỳ diệu. Nó kích thích và truyền cảm hứng. Nó khiến người ta đau khổ nhưng rất đỗi ngọt ngào. Tình yêu là khi anh và em cùng nhau chung sống. Chúc một mùa Valentine hạnh phúc sẽ đến với hai ta."
 
 os.system("cls")
 for i in love:
 	print(i, flush=True)
 	time.sleep(0.1)
+
+for i in word:
+	print(i, flush=True, end="")
+	time.sleep(0.1)
+
+input()
